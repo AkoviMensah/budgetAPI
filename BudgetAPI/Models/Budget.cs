c444bf2015950529870da3d64812f7c9a6641efa
Akovi Mensah <akovi66@gmail.com>
2023-09-09 17:27:31 -0500

    initial commit

---
+++ BudgetAPI/Models/Budget.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetAPI.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Max { get; set; }
+    }
+}
---
+++ BudgetAPI/Models/Expense.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetAPI.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public int BudgetId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
